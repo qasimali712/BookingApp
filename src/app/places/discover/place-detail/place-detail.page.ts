7588d0e81ea95693d3766e22fde5d164130ddc07
qasimali712 <qasimalia712@gmail.com>
2023-05-30 10:30:48 +0500

    App

--- src/app/places/discover/place-detail/place-detail.page.ts
+++ src/app/places/discover/place-detail/place-detail.page.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { ActionSheetController, LoadingController, ModalController, NavController } from '@ionic/angular';
+import {
+  ActionSheetController,
+  LoadingController,
+  ModalController,
+  NavController,
+} from '@ionic/angular';
 import { Place } from '../../place.model';
 import { PlacesService } from '../../places.service';
 import { CreateBookingComponent } from '../../../bookings/create-booking/create-booking.component';
@@ -20,22 +25,21 @@ export class PlaceDetailPage implements OnInit {
     private loadingCtrl: LoadingController,
     private actionCtrl: ActionSheetController,
     private bookingSer: BookingService
-  ) { }
+  ) {}
   ngOnInit() {
     this.place = this.placeService.places;
     console.log(this.place);
   }
 
-
   onBook() {
-
-    this.actionCtrl.create({
-      header: 'Choose an Action',
-     // cssClass: 'my-custom-class',
-      buttons: [
-        {
-          text: 'Select Date',
-          icon: 'calendar',
+    this.actionCtrl
+      .create({
+        header: 'Choose an Action',
+        // cssClass: 'my-custom-class',
+        buttons: [
+          {
+            text: 'Select Date',
+            icon: 'calendar',
             handler: () => {
               this.openBook('select');
             },
@@ -43,60 +47,61 @@ export class PlaceDetailPage implements OnInit {
           {
             text: 'Random Date',
             icon: 'shuffle',
-          handler: () => {
-            this.openBook('random')
-          }
-        },
-        {
-          text: 'Cancel',
-          role: 'cancel',
-          data: {
-            action: 'cancel',
+            handler: () => {
+              this.openBook('random');
+            },
           },
-        },
-      ],
-    }).then(actionEl => {
-      actionEl.present();
-    });
-
+          {
+            text: 'Cancel',
+            role: 'cancel',
+            data: {
+              action: 'cancel',
+            },
+          },
+        ],
+      })
+      .then((actionEl) => {
+        actionEl.present();
+      });
 
     // this.router.navigateByUrl('/places/tabs/discover');
     //  this.navCrtl.navigateBack('/places/tabs/discover');
   }
   openBook(mode: 'select' | 'random') {
     console.log(mode);
-    this.modalCtrl.create({
-      component: CreateBookingComponent,
-      componentProps: { selectedPlace: this.place }
-    })
-      .then(modalEl => {
-        modalEl.present();
-        return modalEl.onDidDismiss()
+    this.modalCtrl
+      .create({
+        component: CreateBookingComponent,
+        componentProps: { selectedPlace: this.place },
       })
-      .then(result => {
-       // console.log(result.data, result.role);
+      .then((modalEl) => {
+        modalEl.present();
+        return modalEl.onDidDismiss();
+      })
+      .then((result) => {
+        // console.log(result.data, result.role);
         if (result.role === 'confirm') {
-          this.loadingCtrl.create({message:'Booking place...'}).then(loadingEl=>
-            {
+          this.loadingCtrl
+            .create({ message: 'Booking place...' })
+            .then((loadingEl) => {
               loadingEl.present();
               const data = result.data.bookingData;
-              this.bookingSer.addBooking(
-              data.placeId,
-              data.placeTitle,
-              data.placeImage,
-              data.firstName,
-              data.lastName,
-              data.guestNumber,
-              data.startDate,
-              data.endDate
-              ).subscribe(()=>{
-                loadingEl.dismiss();
-              });
+              this.bookingSer
+                .addBooking(
+                  data.placeId,
+                  data.placeTitle,
+                  data.placeImage,
+                  data.firstName,
+                  data.lastName,
+                  data.guestNumber,
+                  data.startDate,
+                  data.endDate
+                )
+                .subscribe(() => {
+                  loadingEl.dismiss();
+                });
             });
-
-        };
-      })
-
+        }
+      });
   }
 }
-
